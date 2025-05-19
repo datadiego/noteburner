e9298e7918a0c0948cd83df82e3c09f44a5783dd
datadiego <juandiegomariscal@gmail.com>
2025-05-20 01:01:22 +0200

    conversion a esm

--- index.js
+++ index.js
@@ -1,60 +1,59 @@
-//░█▀▄░█▀▀░█▀█░█▀▀░█▀█░█▀▄░█▀▀░█▀█░█▀▀░▀█▀░█▀█░█▀▀
-//░█░█░█▀▀░█▀▀░█▀▀░█░█░█░█░█▀▀░█░█░█░░░░█░░█▀█░▀▀█
-//░▀▀░░▀▀▀░▀░░░▀▀▀░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀
+// ░█▀▄░█▀▀░█▀█░█▀▀░█▀█░█▀▄░█▀▀░█▀█░█▀▀░▀█▀░█▀█░█▀▀
+// ░█░█░█▀▀░█▀▀░█▀▀░█░█░█░█░█▀▀░█░█░█░░░░█░░█▀█░▀▀█
+// ░▀▀░░▀▀▀░▀░░░▀▀▀░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀
 
-const crypto = require('crypto');
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const cron = require('node-cron');
-const { port, createFile, scheduleFileDeletion, deleteAllFiles, encrypt, decrypt } = require('./scripts/utils.js');
+import crypto from 'crypto'
+import fs from 'fs'
+import path from 'path'
+import express from 'express'
+import { port, createFile, scheduleFileDeletion, deleteAllFiles, encrypt, decrypt, createNoteDir } from './scripts/utils.js'
+import { fileURLToPath } from 'url'
 
+// ░█▀▀░█▀█░█▀█░█▀▀░▀█▀░█▀▀
+// ░█░░░█░█░█░█░█▀▀░░█░░█░█
+// ░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀
 
-//░█▀▀░█▀█░█▀█░█▀▀░▀█▀░█▀▀
-//░█░░░█░█░█░█░█▀▀░░█░░█░█
-//░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+createNoteDir()
+export const app = express()
+app.use(express.text({ limit: '1mb' }))
+app.use(express.static('public'))
+const AESKey = crypto.randomBytes(32)
+deleteAllFiles(path.join(__dirname, 'notas'))
 
-const app = express();
-app.use(express.text({limit: '1mb'}));
-app.use(express.static('public'));
-const AESKey = crypto.randomBytes(32);
-deleteAllFiles(path.join(__dirname, 'notas'));
-
-//░█▀▀░█▀█░█▀▄░█▀█░█▀█░▀█▀░█▀█░▀█▀░█▀▀
-//░█▀▀░█░█░█░█░█▀▀░█░█░░█░░█░█░░█░░▀▀█
-//░▀▀▀░▀░▀░▀▀░░▀░░░▀▀▀░▀▀▀░▀░▀░░▀░░▀▀▀
+// ░█▀▀░█▀█░█▀▄░█▀█░█▀█░▀█▀░█▀█░▀█▀░█▀▀
+// ░█▀▀░█░█░█░█░█▀▀░█░█░░█░░█░█░░█░░▀▀█
+// ░▀▀▀░▀░▀░▀▀░░▀░░░▀▀▀░▀▀▀░▀░▀░░▀░░▀▀▀
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'simple.html'));
-});
+  res.sendFile(path.join(__dirname, 'public', 'simple.html'))
+})
 
 app.post('/', async (req, res) => {
-    try {
-        const id = crypto.randomBytes(16).toString('hex');
-        const raw_data = req.body;
-        const data = encrypt(raw_data, id, AESKey); 
-        const filePath = path.join(__dirname, 'notas', `${id}`);
-        const respuesta = await createFile(filePath, data, res);
-        scheduleFileDeletion(filePath);
-        res.send(respuesta);
-    } catch (error) {
-        console.error('Error en el endpoint POST /:', error);
-        res.status(500).send('Error interno del servidor');
-    }
-});
+  try {
+    const id = crypto.randomBytes(16).toString('hex')
+    const rawData = req.body
+    const data = encrypt(rawData, id, AESKey)
+    const filePath = path.join(__dirname, 'notas', `${id}`)
+    const respuesta = await createFile(filePath, data, res)
+    scheduleFileDeletion(filePath)
+    res.send(respuesta)
+  } catch (error) {
+    console.error('Error en el endpoint POST /:', error)
+    res.status(500).send('Error interno del servidor')
+  }
+})
 
 app.get('/:id', (req, res) => {
-    const id = req.params.id;
-    const filePath = path.join(__dirname, 'notas', `${id}`);
-    const encrypted = fs.readFileSync(filePath, 'utf8');
-    const decrypted = decrypt(encrypted, id, AESKey);
-    res.setHeader('Content-Type', 'text/plain');
-    res.send(decrypted);
-});
+  const id = req.params.id
+  const filePath = path.join(__dirname, 'notas', `${id}`)
+  const encrypted = fs.readFileSync(filePath, 'utf8')
+  const decrypted = decrypt(encrypted, id, AESKey)
+  res.setHeader('Content-Type', 'text/plain')
+  res.send(decrypted)
+})
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-
-
-module.exports = app;
+  console.log(`Server is running on http://localhost:${port}`)
+})
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,95 +1,105 @@
-const fs = require('fs');
-const crypto = require('crypto');
-const path = require('path');
-const cron = require('node-cron');
-const port = 8000;
+import fs from 'fs'
+import crypto from 'crypto'
+import path from 'path'
+import cron from 'node-cron'
+import { fileURLToPath } from 'url'
+export const port = 8000
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 /**
  * Crea un archivo con la data proporcionada y devuelve la URL del archivo al cliente
  * @param {*} filePath Ruta del archivo a crear
  * @param {*} data Texto a escribir en el archivo
  * @param {*} res Respuesta HTTP
  */
-async function createFile(filePath, data, res) {
-    console.log(`Creando archivo ${filePath} a las ${new Date().toLocaleTimeString()}`);
-    return new Promise((resolve, reject) => {
-        fs.writeFile(filePath, data, (err) => {
-            if (err) {
-                console.error(`Error al crear el archivo ${filePath}:`, err);
-                reject(err);
-                return;
-            }            
-            resolve(`http://localhost:${port}/${path.basename(filePath)}`);
-        });
-    });
+export async function createFile (filePath, data, res) {
+  console.log(`Creando archivo ${filePath} a las ${new Date().toLocaleTimeString()}`)
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, data, (err) => {
+      if (err) {
+        console.error(`Error al crear el archivo ${filePath}:`, err)
+        reject(err)
+        return
+      }
+      resolve(`http://localhost:${port}/${path.basename(filePath)}`)
+    })
+  })
 }
 
 /**
  * Programa la eliminación de un archivo
  * @param {*} filePath Ruta del archivo a eliminar
  */
-function scheduleFileDeletion(filePath) {
-    //cada minuto * * * * *
-    //cada hora   0 * * * *
-    //cada dia    0 0 * * *
-    const task = cron.schedule('* * * * *', () => {
-        fs.stat(filePath, (err, stats) => {
-            console.log(`Verificando si el archivo ${filePath} debe ser eliminado`);
-            if (err) {
-                console.error(`Error al obtener información del archivo ${filePath}:`, err);
-                return;
-            }
-            const now = new Date().getTime();
-            const createTime = new Date(stats.ctime).getTime();
-            //60000 = 1 minuto
-            //3600000 = 1 hora
-            //86400000 = 24 horas
-            const endTime = createTime + 60000;
-            if (now > endTime) {
-                fs.unlink(filePath, (err) => {
-                    if (err) {
-                        console.error(`Error al eliminar el archivo ${filePath}:`, err);
-                    } else {
-                        console.log(`Archivo ${filePath} eliminado después de 1 minuto`);
-                    }
-                });
-                task.stop(); // Detener la tarea cron después de eliminar el archivo
-            }
-        });
-    });
+export function scheduleFileDeletion (filePath) {
+  // cada 5 minutos */5 * * * *
+  // cada minuto * * * * *
+  // cada hora   0 * * * *
+  // cada dia    0 0 * * *
+  const task = cron.schedule('0 0 * * *', () => {
+    fs.stat(filePath, (err, stats) => {
+      console.log(`Verificando si el archivo ${filePath} debe ser eliminado`)
+      if (err) {
+        console.error(`Error al obtener información del archivo ${filePath}:`, err)
+        return
+      }
+      const now = new Date().getTime()
+      const createTime = new Date(stats.ctime).getTime()
+      // 60000 = 1 minuto
+      // 3600000 = 1 hora
+      // 86400000 = 24 horas
+      const endTime = createTime + 60000
+      if (now > endTime) {
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.error(`Error al eliminar el archivo ${filePath}:`, err)
+          } else {
+            console.log(`Archivo ${filePath} eliminado después de 1 minuto`)
+          }
+        })
+        task.stop() // Detener la tarea cron después de eliminar el archivo
+      }
+    })
+  })
 }
 
-function deleteAllFiles(path){
-    fs.readdir(path, (err, files) => {
+export function deleteAllFiles (path) {
+  fs.readdir(path, (err, files) => {
+    if (err) {
+      console.error(`Error al leer el directorio ${path}:`, err)
+      return
+    }
+    files.forEach(file => {
+      fs.unlink(path + '/' + file, err => {
         if (err) {
-            console.error(`Error al leer el directorio ${path}:`, err);
-            return;
+          console.error(`Error al eliminar el archivo ${file}:`, err)
+        } else {
+          console.log(`Archivo ${file} eliminado`)
         }
-        files.forEach(file => {
-            fs.unlink(path + '/' + file, err => {
-                if (err) {
-                    console.error(`Error al eliminar el archivo ${file}:`, err);
-                } else {
-                    console.log(`Archivo ${file} eliminado`);
-                }
-            });
-        });
-    });
+      })
+    })
+  })
 }
 
-function encrypt(data, id, key) {
-    const iv = Buffer.from(id, 'hex');
-    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-    let encrypted = cipher.update(data, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
-    return encrypted;
+export function encrypt (data, id, key) {
+  const iv = Buffer.from(id, 'hex')
+  const cipher = crypto.createCipheriv('aes-256-cbc', key, iv)
+  let encrypted = cipher.update(data, 'utf8', 'hex')
+  encrypted += cipher.final('hex')
+  return encrypted
 }
 
-function decrypt(data, id, key) {
-    const iv = Buffer.from(id, 'hex');
-    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-    let decrypted = decipher.update(data, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
-    return decrypted;
+export function decrypt (data, id, key) {
+  const iv = Buffer.from(id, 'hex')
+  const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv)
+  let decrypted = decipher.update(data, 'hex', 'utf8')
+  decrypted += decipher.final('utf8')
+  return decrypted
 }
 
-module.exports = { port, createFile, scheduleFileDeletion, deleteAllFiles, encrypt, decrypt };
+export function createNoteDir () {
+  const dirPath = path.join(__dirname, 'notas')
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath)
+  }
+}
